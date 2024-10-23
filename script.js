4d094e1e0634fa46ff973e606231a918bb1d92aa
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-10-23 09:42:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//---------- REVISÃO ----------
+//var dia = 20 <-- não é mais utilizada
+
+let nome = "vinicius"
+nome = "marina" //<-- tipo string
+
+console.log(nome)
+
+//const não dá para mudar depois
+const rg = 18562537 //<-- tipo number
+//rg = 2222222
+
+console.log(rg)
+
+let dia = true //<-- tipo boolean (true ou false)
+
+let aluno //<-- tipo undefined (não defini oq ele é)
+
+ console.log(aluno)
+
+ let noite = null // <-- tipo nulo (é uma caixa que está ali mas não está sendo utilizada ainda)
+
+ console.log(5/"y") //<-- tipo not a number (NaN)
+
+ //---------- FIM DA REVISÃO ----------
