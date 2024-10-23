1e458285ce31bbd8a4d657acd1351384a3f428d6
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-10-23 10:13:10 -0300

    adic. operadores de comparacao

--- script.js
+++ script.js
@@ -4,22 +4,54 @@
 let nome = "vinicius"
 nome = "marina" //<-- tipo string
 
-console.log(nome)
+//console.log(nome)
 
 //const não dá para mudar depois
 const rg = 18562537 //<-- tipo number
 //rg = 2222222
 
-console.log(rg)
+//console.log(rg)
 
 let dia = true //<-- tipo boolean (true ou false)
 
 let aluno //<-- tipo undefined (não defini oq ele é)
 
- console.log(aluno)
+//console.log(aluno)
 
- let noite = null // <-- tipo nulo (é uma caixa que está ali mas não está sendo utilizada ainda)
+let noite = null // <-- tipo nulo (é uma caixa que está ali mas não está sendo utilizada ainda)
 
- console.log(5/"y") //<-- tipo not a number (NaN)
+//console.log(5/"y") //<-- tipo not a number (NaN)
 
- //---------- FIM DA REVISÃO ----------
+//---------- FIM DA REVISÃO ----------
+
+//---------- OPERADORES DE COMPARAÇÃO ----------
+// = atribuição de tipo
+// == verificação de valor
+// === verifica o tipo e o valor
+// != verifica se os valores são diferentes
+// !== verifica se os valores são restritamente diferentes
+// > maior que
+// < menor que
+// >= maior ou igual que
+// <= menor ou igual que
+
+
+let number = 2
+let numero = "2"
+
+//console.log(number == numero)
+//console.log(number === numero) 
+//console.log(numero != number)
+//console.log(numero !== number)
+//console.log(numero >= number)
+//console.log(numero <= number)
+
+//if(condição){
+//  console.log("retorno")
+//}
+
+let idade = 24
+
+if(idade > 18){
+    console.log("Você é maior de idade!")
+}
